9409ecbf2e78a170f2d80e0211321f236d12545b
Patrick Keaveny <keavenyp@gmail.com>
2014-03-04 11:43:56 +0100

    Added Css
    Added the option to create a CSS file based on an existing template.

--- website_creator.py
+++ website_creator.py
@@ -16,6 +16,16 @@ def _create_index_page(file_path, contents):
         out_file.write(contents)
 
 '''
+Function for creating css file
+    @file_path = the directory/folder where the index should go
+    @contents = contents to fill the index page with
+'''
+def _create_css_page(file_path, contents):
+    file1 = file_path + '/style.css'
+    with open(file1, 'w+') as out_file:
+        out_file.write(contents)
+
+'''
 Function for creating the contents of the index
     @file_path = file to read as a template
 '''
@@ -33,12 +43,21 @@ The main method
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("file_path", help="This argument specifies the file path where the outputted web page will go.")
-    parser.add_argument("-t", "--template_file", help="This argument specifies the file path where the template is drawn from.")
+    parser.add_argument("--t", "--index_template",
+                        help="This argument specifies the file path where the index template is drawn from.")
+    parser.add_argument("--v", "--css_template",
+                        help="This argument specifies the file path where the css template is drawn from.")
     args = parser.parse_args()
     path = args.file_path
-    if args.template_file:
-        contents = str(_contents(args.template_file))
+    index_page = args.t
+    css_page = args.v
+    if index_page:
+       index_contents = str(_contents(index_page))
     else:
         contents = 'Hello World!'
-    _create_index_page(path, contents)
- #   _create_css(contents)
+    if css_page:
+        css_contents = str(_contents(css_page))
+    else:
+        contents = 'body { margin: 0; } '
+    _create_index_page(path, index_contents)
+    _create_css_page(path, css_contents)
