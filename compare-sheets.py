854f0c01c0efdf9549f7cbd5bc3f35c9c6dfd05a
Patrick Keaveny <keavenyp@gmail.com>
2014-03-04 16:05:28 +0100

    Optimized Extensibility
    Created more generic variable names so script could be used for a wider
    variety of tasks. Also made the "output" directory and optional one.

--- compare-sheets.py
+++ compare-sheets.py
@@ -1,6 +1,7 @@
 '''
 This is a script that takes two files and determines whether they are the same
 If not, the deviant values are written to an output file
+User may specify an optional directory to place output file in
 Patrick Keaveny , 2014
 '''
 
@@ -8,20 +9,24 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", help="This argument specifies the file to read")
-    parser.add_argument("file_path_two", help="This argument specifies the file to write to")
-    parser.add_argument("file_path_three", help="This argument specifies the file to compare")
+    parser.add_argument("file_path_one",
+                        help="This argument specifies the file to read")
+    parser.add_argument("file_path_two",
+                        help="This argument specifies the file to compare")
+    parser.add_argument("--v", "--file_path_two",
+                        help="This is an optional argument if you want to write the output to a specific directory")
     args = parser.parse_args()
-    path_one = args.file_path
-    path_three = args.file_path_three
-    file_one = open(path_one, 'r')
-    path_two = args.file_path_two
-    file_three = open(path_three, 'r')
-    all_users = file_one.readlines()
-    written_users = file_three.readlines()
-    with open(path_two, 'w') as out_file:
-        for user in all_users:
-            if user not in written_users:
+    file_one = open(args.file_path_one, 'r')
+    file_two = open(args.file_path_two, 'r')
+    users_first_set = file_one.readlines()
+    users_second_set = file_two.readlines()
+    if args.v:
+        output_file = args.v + '/output.txt'
+    else:
+        output_file = 'output.txt'
+    with open(output_file, 'w+') as out_file:
+        for user in users_first_set:
+            if user not in users_second_set:
                 out_file.write(str(user))
 
 
